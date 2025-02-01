ea77997dde646af6ee822aafbe328c6c233ab7f6
Diego Gómez <diego.polo@rappi.com>
2025-02-01 18:33:41 -0500

    basic init

---
+++ implementations/ml/knn/implementation.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void knn()
+{
+    printf("knn\n");
+}
---
+++ implementations/ml/knn/knn.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "knn.h"
+
+inline __attribute__((always_inline)) void knn_2()
+{
+    int i = 2 + 3;
+    printf("knn_2 %d\n", i);
+}
+
+int main()
+{
+    knn();
+    knn_2();
+    return 0;
+}
